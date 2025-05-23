9cad00bb6b565812ae441622dd2d4ddeac91cbaf
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2025-05-23 18:52:15 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import HorizontalScrollCarousel  from "@/app/_components/HorizontalScrollCarousel";
+
+export default function Home() {
+   return (
+     <HorizontalScrollCarousel />
+   );
+}
+
