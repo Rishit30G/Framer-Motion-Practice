3f6e8334921f57e6ba6266a39afa475a07dd228c
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2025-05-25 16:38:29 +0530

    Add Slide Tabs

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,11 @@ import { RevealLinks } from "@/app/_components/RevealLinks";
 import DrawCircleText from "@/app/_components/DrawCircleText";
 import ScrollStack from "@/app/_components/ScrollAnimation";
 import SmoothScrollSection from "@/app/_components/ScrollSection";
+import SlideTabsExample from "@/app/_components/SlideTabs";
 
 export default function Home() {
    return (
+    <></>
    );
 }
 
